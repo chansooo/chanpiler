5374c3017531a3e92a1cc336f8adae48e2714ab6
chansooo <kcs05008@gmail.com>
2022-05-23 16:13:23 +0900

    make LRTable

---
+++ src/DecodeTable.java
@@ -0,0 +1,71 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class DecodeTable {
+    private JSONObject slrTable;
+    private JSONObject transTable;
+    private JSONObject ruleTable;
+    private JSONParser parser = new JSONParser();
+
+    public DecodeTable(){
+        DecodeSLRTable();
+        DecodeTransTable();
+        DecodeRuleTable();
+    }
+
+    public void DecodeSLRTable(){
+        try{
+            InputStream l = getClass().getResourceAsStream("/LRTable.json");
+            slrTable = (JSONObject) parser.parse(new InputStreamReader(l));
+        } catch (Exception error){
+            System.out.println(error);
+        }
+    }
+    public void DecodeTransTable(){
+        try{
+            InputStream a = getClass().getResourceAsStream("./transTable.json");
+            transTable = (JSONObject) parser.parse(new InputStreamReader(a));
+        } catch (Exception error){
+            System.out.println(error);
+        }
+    }
+
+    public  void DecodeRuleTable(){
+        try{
+            InputStream b = getClass().getResourceAsStream("./transTable.json");
+            transTable = (JSONObject) parser.parse(new InputStreamReader(b));
+        } catch (Exception error){
+            System.out.println(error);
+        }
+    }
+
+//    public void DecodeTable(){
+//        try{
+//            InputStream l = getClass().getResourceAsStream("/LRTable.json");
+//            slrTable = (JSONObject) parser.parse(new InputStreamReader(l));
+//        } catch (Exception error){
+//            System.out.println(error);
+//        }
+//    }
+
+//    public JSONObject getTable(int tableNum){
+//        String tableID = Integer.toString(tableNum);
+//        JSONObject parsedTable = (JSONObject) table.get(tableID);
+//        return parsedTable;
+//    }
+
+    public JSONObject getTransTable() {
+        return transTable;
+    }
+
+    public JSONObject getSlrTable() {
+        return slrTable;
+    }
+
+    public JSONObject getRuleTable(){
+        return ruleTable;
+    }
+}
